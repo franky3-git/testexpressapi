32214dfe58d51a003db4518b93c71e95437d9d76
franky3-git <frankndounga@yahoo.fr>
2021-02-11 10:49:40 +0100

    fix some bugs

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const log = console.log;
 const path = require('path');
-//const cors = require('cors');
 require('dotenv').config({path: 'conf'});
 const port = process.env.PORT || 3000;
 
@@ -8,14 +7,6 @@ const express = require('express');
 let { html } = require('./mainFile');
 const api = require('./routes/api');
 
-/*
-const users = [
-	{"name": "frank", "password": "1234", "email": "thisisemail@gmail.com", "id":1},
-	{"name": "armel", "password": "4569", "email": "ytayops@gmail.com", "id":2},
-	{"name": "ulrich", "password": "8752", "email": "keutchatang@gmail.com", "id": 3},
-	{"name": "melissa", "password": "4156", "email": "tchopteyouM@gmail.com", "id": 4}
-]
-*/
 
 const server = express();
 
